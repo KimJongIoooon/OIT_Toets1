4e7b811d0081b8d9411b167b8e294cd9e943b65a
Iain den Houting <iain1998@live.nl>
2019-06-15 00:54:34 +0200

    Finished

--- OIT_Toets1/OIT_Toets1.ino
+++ OIT_Toets1/OIT_Toets1.ino
@@ -11,60 +11,52 @@
 // Pins
 const int potPin = A0;    // select the input pin for the potentiometer
 const int redLedPin = 13;      // pin for the red LED
-const int greenLedPin = 12;    // pin for the Green LED
-const int buttonPin = 11;
+const int greenLedPin = 11;    // pin for the Green LED
+const int buttonPin = 12;
 
 //Vault
-int vaultCode[] = {22, 1, 30, 0};
+bool codeSet = false; //true to hardcode the vaultcode
+int vaultCode[] = {1, 1, 1, 1};
 int inputCode[] = {0, 0, 0, 0};
 
-//input
+
+bool lastButtonState;
+  
+
 int inputNumber;    //number seletected by user
+int lastInputNumber = 50;
+
 int numbersEntered; //amount of numbers guessed by user.
 
-//button debouncing
-bool codeSet = false;
-bool buttonTriggered;
-bool buttonState;
-bool lastButtonState;
-long lastDebounceTime = 0;
-bool trigger = false;
 
 
-void readButtonPress() {
-  // read the pushbutton input pin:
-  buttonState = digitalRead(buttonPin);
-
-  // compare the buttonState to its previous state
+bool debounceButton(){
+  
+}
+bool readButtonTrigger() {
+  bool buttonState = digitalRead(buttonPin);
+  bool trigger = false;
   if (buttonState != lastButtonState) {
     // if the state has changed, increment the counter
     if (buttonState == HIGH) {
-      // if the current state is HIGH then the button went from off to on
-      trigger = HIGH;
-    } else {
-      // if the current state is LOW then the button went from on to off:
-
-    }
-    // Delay a little bit to avoid bouncing
-    delay(50);
+      trigger = true;
+    } 
+    delay(50);//debounce delay
   }
+  
   // save the current state as the last state, for next time through the loop
   lastButtonState = buttonState;
+  return trigger;  
 }
 
 int readInputNumber()
 {
   int potValue = analogRead(potPin);
-  return map(potValue, 0, 1023, 0, 25);
+  return inputNumber =map(potValue, 0, 1023, 0, 25);
 }
 
 void displayVaultCode()
 {
-
-  if (numbersEntered >= 0)
-  {
-
-  }
   if (numbersEntered >= 1)
   {
     Serial.print(vaultCode[0]);
@@ -87,10 +79,7 @@ void displayVaultCode()
 void displayInputCode()
 {
   Serial.print("Enter Vault Code");
-  if (numbersEntered >= 0)
-  {
 
-  }
   if (numbersEntered >= 1)
   {
     Serial.print(inputCode[0]);
@@ -106,30 +95,45 @@ void displayInputCode()
     Serial.print(inputCode[2]);
     Serial.print("-");
   }
+  if (numbersEntered < 4) {
+    Serial.println(inputNumber);
+  }
+}
+bool checkCode() {
 
-  Serial.println(inputNumber);
-}
-void reset(){
-  numbersEntered = 0;
-}
-void succes(){
-  digitalWrite(redLedPin, LOW);
-  delay(500);
-  digitalWrite(redLedPin, HIGH);
-  delay(500);
-  digitalWrite(redLedPin, LOW);
-  delay(500);
-  digitalWrite(redLedPin, HIGH);
-  delay(500);
-  digitalWrite(redLedPin, LOW);
-  delay(500);
-  digitalWrite(redLedPin, HIGH);
-  delay(500);
-  digitalWrite(redLedPin, LOW);
-  delay(500);
-}
-void Fail(){
+  Serial.print(inputCode[0]);
+  Serial.print("-");
+  bool wrong = true;
   
+  for (int i = 0; i <= 2; i++) {
+    if (!(inputCode[i] == vaultCode[i])) {
+      wrong = false;
+      break;
+    }
+  }
+  return wrong;
+}
+void reset() {
+  numbersEntered = 0;
+  lastInputNumber = 50;//Out of range 0-25 so that is diffrent in next cyclus, so display will show at start
+  digitalWrite(greenLedPin, LOW);
+  digitalWrite(redLedPin, LOW);
+}
+void succes() {
+  Serial.println("kluis Open");
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(greenLedPin, HIGH);
+  delay(3000);
+}
+void fail() {
+  Serial.println("kluis dicht");
+  digitalWrite(greenLedPin, LOW);
+  for (int i = 0; i <= 2; i++) {
+    digitalWrite(redLedPin, LOW);
+    delay(500);
+    digitalWrite(redLedPin, HIGH);
+    delay(500);
+  }
 }
 void setup()
 {
@@ -139,7 +143,7 @@ void setup()
   // I/O
   pinMode(redLedPin, OUTPUT);
   pinMode(greenLedPin, OUTPUT);
-
+  pinMode(buttonPin, INPUT);
   //blink red green
   digitalWrite(redLedPin, HIGH);
   delay(400);
@@ -149,51 +153,71 @@ void setup()
   digitalWrite(greenLedPin, LOW);
 }
 
-
 void loop()
 {
   // read the value from the sensor:
-  inputNumber = readInputNumber();
-  readButtonPress();
+
+
 
   if (!codeSet)
   {
     digitalWrite(redLedPin, HIGH);
     digitalWrite(greenLedPin, HIGH);
-    if (trigger == HIGH)
+    inputNumber = readInputNumber();
+    if (lastInputNumber != inputNumber){
+      Serial.print("Stel uw code in:");
+      displayVaultCode();
+    }
+    lastInputNumber = inputNumber;
+    if (readButtonTrigger())
     {
       vaultCode[numbersEntered] = inputNumber;
       numbersEntered++;
-      trigger = LOW;
+      lastInputNumber =50;
+      
     }
     if (numbersEntered > 3)
     {
-      codeSet = True;
+      
       Serial.print("Code is ");
       displayVaultCode();
-    } else
-    {
-      Serial.print("Set Vault Code ");
-      displayVaultCode();
-    }
-
+      lastInputNumber = 50;
+      delay(2500);
+      numbersEntered=0;
+      codeSet = true;
+    } 
   }
-
 
   if (codeSet)
   {
-    if (numbersEntered < 4)
-    {
-      digitalWrite(redLedPin, HIGH);
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(greenLedPin, LOW);
+    inputNumber = readInputNumber();
+    if (lastInputNumber != inputNumber){
+      Serial.print("Vul uw code in:");
+      displayVaultCode();
     }
-    if (trigger == HIGH)
+    lastInputNumber = inputNumber;
+    if (readButtonTrigger())
     {
       inputCode[numbersEntered] = inputNumber;
+      lastInputNumber = 50;
       numbersEntered++;
-      trigger = LOW;
     }
-
-    displayInputCode();
+    if (numbersEntered < 3)
+    {
+      digitalWrite(redLedPin, HIGH);
+      
+    }
+    if (numbersEntered > 3)
+    {
+      bool codeCorrect = checkCode();
+      if(codeCorrect){
+        succes();    
+      } else {
+        fail();
+      }
+      reset();
+    }
   }
-
 }
